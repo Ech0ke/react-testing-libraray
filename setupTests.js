01fcd62a53dc480ac4d1b9e5970e3ae8b08426e2
Ech0ke <aidas.zygas@gmail.com>
2023-09-10 11:21:23 +0300

    Fixed test config error

--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,8 @@
-import { afterEach } from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { afterEach, expect } from "vitest";
 import { cleanup } from "@testing-library/react";
+
+expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
