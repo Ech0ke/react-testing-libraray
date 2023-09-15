8ac7fcfe0928928b12293c6117cabdec00bae622
Ech0ke <aidas.zygas@gmail.com>
2023-09-15 12:46:59 +0300

    Added fake component to be tested

---
+++ src/PostList.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://example.com/api/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <ol>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ol>
+  );
+}
+
+export default PostList;
